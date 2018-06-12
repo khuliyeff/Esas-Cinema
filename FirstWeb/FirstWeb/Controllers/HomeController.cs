68ef8b3c6b3ec73c90b0c30aba74765c9ad3efee
Ali Khuliyev <aliag@code.edu.az>
2018-06-12 18:01:45 +0400

    asd

---
+++ FirstWeb/FirstWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstWeb.Models;
+
+namespace FirstWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        BekarEntities db = new BekarEntities();
+        public ActionResult Index()
+        {
+            List<Result> results = db.Results.ToList();
+            return View(results);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
